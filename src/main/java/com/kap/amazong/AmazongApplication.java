ada692a823bb4bf34ec79df76aef069500711301
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-03-11 11:24:05 -0500

    Initialize Amazong!

---
+++ src/main/java/com/kap/amazong/AmazongApplication.java
@@ -0,0 +1,13 @@
+package com.kap.amazong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kap/amazong/AmazongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kap.amazong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
