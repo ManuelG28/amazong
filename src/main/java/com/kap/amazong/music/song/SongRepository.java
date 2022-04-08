3f9059fdf4b3619a2d5a5290d5af3d8b50e9d03f
Manuel Gutierrez Mejia <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-04-08 14:54:04 -0500

    Fix n+1 problem in Song Entity
    Since we are loading the song genre laziness we needed to figured out how to get the songs with their genres with only one query.
    Using @EntityGraph was the best approach to solve this problem.

--- src/main/java/com/kap/amazong/music/song/SongRepository.java
+++ src/main/java/com/kap/amazong/music/song/SongRepository.java
@@ -1,6 +1,7 @@
 package com.kap.amazong.music.song;
 
 import com.kap.amazong.music.genre.Genre;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface SongRepository extends JpaRepository<Song, Long> {
     Song findSongById(Long id);
 
     List<Song> findSongByGenre(Genre genre);
+
+    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "genre")
+    List<Song> findAll();
 }
